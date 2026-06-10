f322d3e9deffbd309698014af0b32c5effada777
hamyasha90 <harshgoyal14725@gmail.com>
2026-06-10 20:25:34 +0530

    assignment day 10

--- day_9/question_4.c
+++ day_9/question_4.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int size = 5; // You can change this value to make the square bigger or smaller
-    
-    // Outer loop for rows
+    int size = 5; 
+  
     for (int i = 1; i <= size; i++) {
-        
-        // Inner loop for columns
         for (int j = 1; j <= size; j++) {
-            
-            // Print '*' if it's the first/last row or first/last column
+     
             if (i == 1 || i == size || j == 1 || j == size) {
                 printf("*");
             } else {
-                // Print space for the hollow part inside
                 printf(" ");
             }
         }
-        
-        // Move to the next line after printing each row
+  
         printf("\n");
     }
 
